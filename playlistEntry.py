e1ef4b57201e6ab300f39412ae37df8aa422f096
BuildTools <unconfigured@null.spigotmc.org>
2021-10-13 16:04:56 +0200

    add channel id to the Playlist Entry class

--- playlistEntry.py
+++ playlistEntry.py
@@ -1,7 +1,13 @@
 class PlaylistEntry:
-    def __init__(self, stream_link, video_name, thumbnail, duration, video_id):
+    def __init__(self, stream_link, video_name, thumbnail, duration, video_id, channel_id):
         self.stream_link = stream_link
         self.video_name = video_name
         self.thumbnail = thumbnail
         self.duration = duration
         self.vid = video_id
+        self.channel_id = channel_id
+
+
+class DirectStream:
+    def __init__(self, stream_link):
+        self.stream_link = stream_link
