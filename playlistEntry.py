d189e189aac837ea7a93921576916b0ea92166f8
BuildTools <unconfigured@null.spigotmc.org>
2021-10-13 16:07:50 +0200

    add "tags" to the Playlist Entry class

--- playlistEntry.py
+++ playlistEntry.py
@@ -1,11 +1,12 @@
 class PlaylistEntry:
-    def __init__(self, stream_link, video_name, thumbnail, duration, video_id, channel_id):
+    def __init__(self, stream_link, video_name, thumbnail, duration, video_id, channel_id, tags):
         self.stream_link = stream_link
         self.video_name = video_name
         self.thumbnail = thumbnail
         self.duration = duration
         self.vid = video_id
         self.channel_id = channel_id
+        self.tags = tags
 
 
 class DirectStream:
